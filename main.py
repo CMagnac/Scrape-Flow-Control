0cfd20295a5cbbe34f3136a078b069c233aed167
cym <pharmadev@proton.me>
2022-11-06 12:41:43 +0100

    Update gitignore, create a scraper object, less effective in execution time

--- main.py
+++ main.py
@@ -1,45 +1,61 @@
 import json
-import random
 import requests
+from requests.exceptions import ConnectionError
 from bs4 import BeautifulSoup as bs
 import time
 
-with open("config.json") as f:
-    cfg = json.load(f)
 
-def extract_data(url=cfg['url']):
-    return requests.get(url).text
-
-def transform_data():
-    soup = bs(extract_data(),"html.parser")
-    return soup
-
-def data_list(soup=transform_data()):
-    new_char = soup.find_all(cfg['html_tag'])
-    return [sino.get_text() for sino in new_char ]
-
-def create_dictionary(datalist=data_list()):
-    sino_list = datalist[0::4]
-    pinyin_list =  datalist[1::4]
-    role_list = datalist[2::4]
-    en_list =  datalist[3::4]
-    return dict(zip(sino_list,zip(pinyin_list,role_list,en_list)))
-
-def control_data_flow(dictionary,N,timelapse):
-    keys_list = list(dictionary.keys())
-    random.shuffle(keys_list)
-    n = 0
-    while n < N:
-        k = keys_list.pop(n)
-        print(f"| {k} | {d[k]} |")
-        print("-"*47)
-        time.sleep(timelapse)
-        n += 1
+class WebScraper():
+    def __init__(self,name,dct) -> None:
+        self.name = name
+        self.configuration = dct
+    
+    def __repr__(self) -> str:
+        return 'WebScraper(' + self.name + ',' + self.configuration['url'] + ')'
+    
+    def data_request(self):
+        """handling web connection
+        """
+        try: 
+            http_response = requests.get(self.configuration['url'])
+            return http_response
+        except ConnectionError as msg:
+            return f"Connection Error :: {msg}"
+    
+    def dictionary(self) -> dict:
+        content = self.data_request()
+        soup = bs(content.text,"html.parser")
+        data = soup.find_all(self.configuration['html_tag'])
+        datalist = [d.get_text() for d in data]
+        sino_list = datalist[0::4]
+        pinyin_list =  datalist[1::4]
+        role_list = datalist[2::4]
+        en_list =  datalist[3::4]
+        return dict(zip(sino_list,zip(pinyin_list,role_list,en_list)))
+    # def control_data_flow(dictionary,N,timelapse):
+#     keys_list = list(dictionary.keys())
+#     random.shuffle(keys_list)
+#     n = 0
+#     while n < N:
+#         k = keys_list.pop(n)
+#         print(f"| {k} | {d[k]} |")
+#         print("-"*47)
+#         time.sleep(timelapse)
+#         n += 1
 
 if __name__ == "__main__":
 
     start = time.time()
-    d = create_dictionary()
-    control_data_flow(d,5,1)
+
+    with open("config.json") as f:
+        cfg = json.load(f)
+    
+    scp = WebScraper("myscraper",cfg)
+    dct = scp.dictionary()
+
+    with open("mydict.json","w") as f:
+        json.dump(dct,f)
+    
     end = time.time()
+
     print(f"EXECUTION TIME : {end-start}")
