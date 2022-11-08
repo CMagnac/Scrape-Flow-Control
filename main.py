4cbac5a64de7efd6372305103825c26ffd39cbbc
cym <pharmadev@proton.me>
2022-11-08 15:08:18 +0100

    fix bug, create new class, add more explanations

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import json
+import random
 import requests
 from requests.exceptions import ConnectionError
 from bs4 import BeautifulSoup as bs
 import time
 
 
-class WebScraper():
+class WebScraper:
     def __init__(self,name,dct) -> None:
         self.name = name
         self.configuration = dct
@@ -32,16 +33,31 @@ class WebScraper():
         role_list = datalist[2::4]
         en_list =  datalist[3::4]
         return dict(zip(sino_list,zip(pinyin_list,role_list,en_list)))
-    # def control_data_flow(dictionary,N,timelapse):
-#     keys_list = list(dictionary.keys())
-#     random.shuffle(keys_list)
-#     n = 0
-#     while n < N:
-#         k = keys_list.pop(n)
-#         print(f"| {k} | {d[k]} |")
-#         print("-"*47)
-#         time.sleep(timelapse)
-#         n += 1
+
+class DataFlowControl:
+    def __init__(self,name,timelapse,data,number) -> None:
+        self.name = name
+        # the time between two data
+        self.timelapse = timelapse
+        # declare a dictionary to store data
+        self.data = data
+        self.number = number
+    def __repr__(self) -> str:
+        return 'DataFlowControl(' + self.name + "," + str(self.timelapse) + "," + str(type(self.data)) + ')'
+    def control(self) -> None:
+        keys_list = list(self.data.keys())
+        random.shuffle(keys_list)
+        if 0 < self.number <= len(self.data):
+            n = 0
+            while n < self.number:
+                k = keys_list.pop(n)
+                print(f"| {k} | {self.data[k]} |")
+                print("-"*43)
+                time.sleep(self.timelapse)
+                n += 1
+        else:
+            print("Value Error")
+            print(f"Expect a number between 1 and {len(self.data)}")
 
 if __name__ == "__main__":
 
@@ -50,11 +66,17 @@ if __name__ == "__main__":
     with open("config.json") as f:
         cfg = json.load(f)
     
-    scp = WebScraper("myscraper",cfg)
+    scp = WebScraper("@scraper",cfg)
     dct = scp.dictionary()
 
+    # save the data into a json file
     with open("mydict.json","w") as f:
         json.dump(dct,f)
+    # print the data with our own values
+    # number refer to the number of sinographs wanted
+    ctrl = DataFlowControl("@dataflow",timelapse=2,data=dct,number=0)
+    
+    ctrl.control()
     
     end = time.time()
 
