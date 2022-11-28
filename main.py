fe65c37443747ac1a802ec6c7ee2dc158794b7cb
cym <pharmadev@proton.me>
2022-11-28 18:58:25 +0100

    Upgrade code readability

--- main.py
+++ main.py
@@ -7,43 +7,48 @@ import time
 
 
 class WebScraper:
-    def __init__(self,name,dct) -> None:
+
+    def __init__(self, name, dct) -> None:
         self.name = name
         self.configuration = dct
-    
+
     def __repr__(self) -> str:
-        return 'WebScraper(' + self.name + ',' + self.configuration['url'] + ')'
-    
+        return 'WebScraper('+self.name+')'
+
     def data_request(self):
         """handling web connection
         """
-        try: 
+        try:
             http_response = requests.get(self.configuration['url'])
             return http_response
         except ConnectionError as msg:
             return f"Connection Error :: {msg}"
-    
+
     def dictionary(self) -> dict:
         content = self.data_request()
-        soup = bs(content.text,"html.parser")
+        soup = bs(content.text, "html.parser")
         data = soup.find_all(self.configuration['html_tag'])
         datalist = [d.get_text() for d in data]
         sino_list = datalist[0::4]
-        pinyin_list =  datalist[1::4]
+        pinyin_list = datalist[1::4]
         role_list = datalist[2::4]
-        en_list =  datalist[3::4]
-        return dict(zip(sino_list,zip(pinyin_list,role_list,en_list)))
+        en_list = datalist[3::4]
+        return dict(zip(sino_list, zip(pinyin_list, role_list, en_list)))
+
 
 class DataFlowControl:
-    def __init__(self,name,timelapse,data,number) -> None:
+
+    def __init__(self, name, timelapse, data, number) -> None:
         self.name = name
         # the time between two data
         self.timelapse = timelapse
         # declare a dictionary to store data
         self.data = data
         self.number = number
+
     def __repr__(self) -> str:
-        return 'DataFlowControl(' + self.name + "," + str(self.timelapse) + "," + str(type(self.data)) + ')'
+        return 'DataFlowControl('+self.name+')'
+
     def control(self) -> None:
         keys_list = list(self.data.keys())
         random.shuffle(keys_list)
@@ -59,25 +64,19 @@ class DataFlowControl:
             print("Value Error")
             print(f"Expect a number between 1 and {len(self.data)}")
 
+
 if __name__ == "__main__":
-
     start = time.time()
-
     with open("config.json") as f:
         cfg = json.load(f)
-    
-    scp = WebScraper("@scraper",cfg)
+    scp = WebScraper("@scraper", cfg)
     dct = scp.dictionary()
-
     # save the data into a json file
-    with open("mydict.json","w") as f:
-        json.dump(dct,f)
+    with open("mydict.json", "w") as f:
+        json.dump(dct, f)
     # print the data with our own values
     # number refer to the number of sinographs wanted
-    ctrl = DataFlowControl("@dataflow",timelapse=2,data=dct,number=0)
-    
+    ctrl = DataFlowControl("@dataflow", timelapse=2, data=dct, number=1)
     ctrl.control()
-    
     end = time.time()
-
     print(f"EXECUTION TIME : {end-start}")
